b7d79e255f4327fa5b9ff8a6242ae613093f7536
lazy-sky <1cielo@naver.com>
2021-10-22 14:19:57 +0900

    Install and test @babel/preset-env

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ const a = 1;
 const abc = () => {
   console.log(a)
 }
+
+class User {
+  constructor(name) {
+    this.name = name
+  }
+
+  getName() {
+    return this.name
+  }
+}
+
+const b = { x: 1}
+const c = {
+  ...b,
+  y: 2
+}

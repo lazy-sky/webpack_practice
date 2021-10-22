35fddf9897e6a66ecead02c738a2b59b2d541ba5
lazy-sky <1cielo@naver.com>
2021-10-22 13:43:04 +0900

    Testing npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 module.exports = (env, options) => {
   return {
-    entry: './main.js',
-    output: {}
+    entry: './src/main.js',
+    output: {
+      // 기본값 설정돼있어서 생략 가능
+      // path: '',
+      // filename: ''
+    }
   }
 }
